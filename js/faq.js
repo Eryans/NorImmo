c764ca7894ef880d02d48d563354a73116bdbeb1
Occelli <occelli.hugo@hotmail.fr>
2021-10-15 11:30:12 +0200

    f.a.q responsiv

--- js/faq.js
+++ js/faq.js
@@ -1,10 +1,8 @@
 
 let btns = document.getElementsByClassName("question-list");
-let answers = document.getElementsByClassName("to-reveal");
 
 for (let btn of btns){
     btn.onclick = function(){
-        console.log(this.lastElementChild);
        this.lastElementChild.classList.toggle("to-reveal");
    }
 }
